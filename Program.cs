c3261460fd520486a900f7dd168fa5a56539d62a
mahendransaminathan <125678754+mahendransaminathan@users.noreply.github.com>
2024-10-09 16:39:05 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,208 +4,135 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Xml.Linq;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Interactions;
 using OpenQA.Selenium.Support.UI;
 using SeleniumExtras.WaitHelpers;
-using static System.Net.Mime.MediaTypeNames;
 
-
-
-namespace TestAutomation
+namespace SeleniumRunner
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-
             // Set up ChromeDriver
             using (IWebDriver driver = new ChromeDriver())
             {
                 // Navigate to a website
-                driver.Navigate().GoToUrl("https://testautomationpractice.blogspot.com/");
+                driver.Navigate().GoToUrl("https://vbdace.com/");
 
-                // Maximize the browser window
+                                // Maximize the browser window
                 driver.Manage().Window.Maximize();
 
-                var checkBox = driver.FindElement(By.XPath("//table[@id='productTable']/tbody/tr[2]"));
+                IWebElement signin = driver.FindElement(By.XPath("(//a[normalize-space()='Sign In'])[1]"));
+                signin.Click();
 
-                var pt = driver.FindElement(By.XPath("//table[@id='productTable']"));
+                IWebElement EmailId = driver.FindElement(By.XPath("(//input[@placeholder='Enter E-mail Id'])[1]"));
 
-                var table = driver.FindElement(By.XPath("//table[@id='productTable']"));
+                // Enter text into the text box
+                EmailId.SendKeys("mahendransaminathen@gmail.com");
 
-                var rowncol = table.FindElements(By.XPath(".//tbody/tr"));
-                var checkboxes = new List<IWebElement>();
+                Thread.Sleep(5000); // Time in milliseconds
+                IWebElement pwd = driver.FindElement(By.XPath("(//input[@placeholder='Enter password'])[1]"));
 
-                var cbSunday = table.FindElement(By.XPath("//label[normalize-space()='Sunday']"));
+                // Enter text into the text box
+                pwd.SendKeys("Saminathan1");
+
+                Thread.Sleep(5000); // Time in milliseconds
+
+                IWebElement login = driver.FindElement(By.XPath("(//button[normalize-space()='Login'])[1]"));
+                login.Click();
+
+                Thread.Sleep(5000); // Time in milliseconds
+
+                // Select an option by visible text
+                //selectElement.SelectByText("Idli Cooker"); // Replace with the option you want to select
+                // Finding an element by ID and clicking it
+                IWebElement element = driver.FindElement(By.XPath("(//h1[@class='product-title'][normalize-space()='Idly Cooker'])[1]"));
+                element.Click();
+
+                // Perform actions (like finding elements, clicking, etc.)
+                Console.WriteLine("Page title is: " + driver.Title);
+
+                Thread.Sleep(5000); // Time in milliseconds
+                IWebElement element1 = driver.FindElement(By.XPath("(//button[normalize-space()='Add to Cart'])[1]"));
+                element1.Click();
+
                 
-                if(!cbSunday.Selected)
-                {
-
-                    cbSunday.Click();
-                }
-                Thread.Sleep(10000);
-
-                var simpleAlert = driver.FindElement(By.XPath("(//button[normalize-space()='Simple Alert'])[1]"));
-
-                simpleAlert.Click();
-
-                // Switch to the alert
-                IAlert alert = driver.SwitchTo().Alert();
-
-                // Get alert text (optional)
-                string alertText = alert.Text;
-                Console.WriteLine("Alert text: " + alertText);
-
-                // Accept the alert (click "OK")
-                alert.Accept();
 
 
-                var pg2 = driver.FindElement(By.XPath("//a[normalize-space()='2']"));
-                pg2.Click();
+                Thread.Sleep(5000); // Time in milliseconds
+                IWebElement Cart = driver.FindElement(By.XPath("(//a[@href='/cart'])[1]"));
+                Cart.Click();
 
-                Thread.Sleep(10000);
+                Thread.Sleep(5000); // Time in milliseconds
 
-                var doubleclick = driver.FindElement(By.XPath("//button[normalize-space()='Copy Text']"));
+                IWebElement co = driver.FindElement(By.XPath("(//button[normalize-space()='Proceed to checkout'])[1]"));
+                co.Click();
 
-                // Create an instance of the Actions class
+                Thread.Sleep(5000); // Time in milliseconds
+
+                IWebElement Pay = driver.FindElement(By.XPath("(//button[@id='checkout-pay-button'])[1]"));
+                Pay.Click();
+
+                IWebElement name = driver.FindElement(By.XPath("(//p[@id='error-for-TextField1'])[1]"));
+
+                string fieldText = name.Text;
+                // Get the text from the field
+                
+
+                // Print the text to the console
+                Console.WriteLine("Text in the field: " + fieldText);
+
+
+                IWebElement Dace = driver.FindElement(By.XPath("(//img)[2]"));
+                Dace.Click();
+
+                Thread.Sleep(5000); // Time in milliseconds
+
+                IWebElement ArrowRight = driver.FindElement(By.XPath("(//img[@alt='Arrow Right'])[1]"));
+                ArrowRight.Click();
+
+                Thread.Sleep(5000); // Time in milliseconds
+
+                IWebElement Products = driver.FindElement(By.XPath("(//a[normalize-space()='Products'])[1]"));
+                //Products.Click();
+
+                // Create an Actions instance
                 Actions actions = new Actions(driver);
 
-                // Perform the double-click action on the button
-                actions.DoubleClick(doubleclick).Perform();
+                // Hover over the menu item
+                actions.MoveToElement(Products).Perform();
 
-                Thread.Sleep(10000);
+                // Create a WebDriverWait instance
+                //WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
 
-                var sortedlist = driver.FindElements(By.XPath("//select[@id='animals']"));
+                // Wait for the dropdown options to be visible
+                //var options = wait.Until(ExpectedConditions.VisibilityOfAllElementsLocatedBy(By.ClassName("dropdown-item"))); // Change the selector
 
-                // Extract texts from the elements into a list
-                List<string> textList = new List<string>();
-                foreach (IWebElement element in sortedlist)
+                // Click the desired option
+                /*foreach (var option in options)
                 {
-                    textList.Add(element.Text);
-                }
-                // Display the extracted texts
-                Console.WriteLine("Extracted texts:");
-                foreach (var text in textList)
-                {
-                    Console.WriteLine(text);
-                }
-
-                // Create a sorted copy of the list
-                List<string> sortedList = new List<string>(textList);
-                sortedList.Sort();
-
-                // Check if the original list is sorted
-                bool isSorted = textList.SequenceEqual(sortedList);
-
-                // Output the result
-                if (isSorted)
-                {
-                    Console.Write("The texts are sorted.");
-                }
-                else
-                {
-                    Console.Write("The texts are not sorted.");
-                }
-
-
-                // Locate the elements for drag and drop
-                var sourceElement = driver.FindElement(By.XPath("//p[normalize-space()='Drag me to my target']")); // Replace with your source selector
-                var targetElement = driver.FindElement(By.XPath("//div[@id='droppable']")); // Replace with your target selector
-
-
-                // Perform the drag and drop action
-                actions.DragAndDrop(sourceElement, targetElement).Perform();
-                Thread.Sleep(10000);
-                /*
-                foreach (var row in rowncol)
-            {
-                var checkbox = row.FindElement(By.XPath(".//input[@type='checkbox']"));
-                checkboxes.Add(checkbox);
-                */
-                //var cells = row.FindElements(By.TagName("td"));
-
-                //var cb = row.FindElement(By.CssSelector("input.rowCheckbox"));
-
-                //foreach (var cell in cells)
-                //{
-                //
-
-                //      if (cb.Selected)
-                //    {
-                //      Console.Write("Yes Its CHecked");
-                //}
-                //}
-
-                /*int count = 0;
-                foreach (var checkbox in checkboxes)
-                {
-                    count++;
-                    if (count == 2)
+                    if (option.Text.Equals("Tableware")) // Change to your desired item text
                     {
-                        checkBox.Click();
-
-                        Console.Write("Yes Its Checked");
+                        option.Click();
                         break;
                     }
-                }
-                Thread.Sleep(10000);
-                checkboxes.Clear();
+                }*/
+                // Select an item by visible text
+                //dropdown.SelectByIndex(1); // Change to the visible text of the option you want to select
 
-                foreach (var row in rowncol)
-                {
-                    var checkbox = row.FindElement(By.XPath(".//input[@type='checkbox']"));
-                    checkboxes.Add(checkbox);
 
-                    //var cells = row.FindElements(By.TagName("td"));
+                IWebElement tw = driver.FindElement(By.XPath("(//a[normalize-space()='Tableware'])[1]"));
+                tw.Click();
 
-                    //var cb = row.FindElement(By.CssSelector("input.rowCheckbox"));
+                Thread.Sleep(5000); // Time in milliseconds
 
-                    //foreach (var cell in cells)
-                    //{
-                    //
-
-                    //      if (cb.Selected)
-                    //    {
-                    //      Console.Write("Yes Its CHecked");
-                    //}
-                    //}
-                }
-
-                foreach (var checkbox in checkboxes)
-                {
-                    if (checkbox.Selected)
-                    {
-                        Console.Write("Yes Its CHecked");
-                    }
-                }
-                */
-                var country = driver.FindElement(By.XPath("//select[@id='country']"));
-
-                var selectElement = new SelectElement(country);
-
-                var options = selectElement.Options;
-                
-                int itemCount = options.Count();
-
-                Console.Write("ItemCount: " + itemCount);
-
-                var table1 = driver.FindElement(By.XPath("//div[@id='HTML1']"));
-
-                var rows = table1.FindElements(By.XPath(".//tbody/tr"));
-
-                foreach (var row in rows)
-                {
-                    var cells = row.FindElements(By.TagName("td"));
-
-                    foreach (var cell in cells)
-                    {
-                        Console.Write(cell.Text + " ");
-                    }
-                }
+                // Close the browser
+                driver.Quit();
             }
+
         }
     }
 }
